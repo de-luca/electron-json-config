109b78a48636aaadb65bc165dab54fec18a50b0f
Kel Phillipson <kel@kelfish.com>
2023-10-01 02:04:20 -0700

    Add `prettier` and `lint-staged` deps to keep files inline with `.editorconfig` (#529)
    * Add prettier as a devDependency
    
    * Add prettier config files
    
    * Format files inline with prettier and editorconfig
    
    * Add husky and lint-staged to enforce prettier
    
    * Refactor where husky scripts are stored
    
    * Tweak pattern for lint-staged in package.json
    
    * Add editorconfig setting to .prettierrc
    
    * Remove husky scripts
    
    * Replace husky with simple-git-hooks for running lint-staged
    
    * Latest package-lock.json after removing npm-run-all as dep
    
    * Remove lint-staged and simple-git-hooks to remove pre-commit checks
    
    * Add extra rules to eslint to trigger errors in CI inline with editorconfig and prettier

--- src/Config.ts
+++ src/Config.ts
@@ -1,23 +1,22 @@
 import Storable from './Storable';
 import * as util from './utils';
 
-
 function sync(
-    _target: Config,
-    _propertyKey: string,
-    descriptor: PropertyDescriptor,
+  _target: Config,
+  _propertyKey: string,
+  descriptor: PropertyDescriptor,
 ): PropertyDescriptor {
-    const originalMethod = descriptor.value;
+  const originalMethod = descriptor.value;
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    descriptor.value = function (this: any, ...args: Array<any>): any {
-        const res = originalMethod.apply(this, args);
-        util.sync(this._file, this._data, this._options.prettyJson);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  descriptor.value = function (this: any, ...args: Array<any>): any {
+    const res = originalMethod.apply(this, args);
+    util.sync(this._file, this._data, this._options.prettyJson);
 
-        return res;
-    };
+    return res;
+  };
 
-    return descriptor;
+  return descriptor;
 }
 
 /**
@@ -30,20 +29,20 @@ export type PrettyJSONConfig = {
 
 /**
  * Options that can be passed to Config for writing and storing data
- * 
- * Currently supports pretty JSON format for storing indented, 
+ *
+ * Currently supports pretty JSON format for storing indented,
  * multi-line in file
  */
 export type ConfigOptions = {
-   prettyJson?: PrettyJSONConfig;
+  prettyJson?: PrettyJSONConfig;
 };
 /**
  * Default config, used for optional `options` args throughout
  */
-export const DEFAULT_CONFIG = { 
-    prettyJson: {
-        enabled: false,
-    },
+export const DEFAULT_CONFIG = {
+  prettyJson: {
+    enabled: false,
+  },
 };
 
 /**
@@ -55,70 +54,68 @@ export const DEFAULT_CONFIG = {
 export type Key = string | Array<string>;
 
 export default class Config {
-    private _file: string;
-    private _data: Storable;
-    private _options: ConfigOptions;
+  private _file: string;
+  private _data: Storable;
+  private _options: ConfigOptions;
 
-    public constructor(
-        file: string, 
-        data: Storable, 
-        options: ConfigOptions = DEFAULT_CONFIG,
-    ) {
-        this._file = file;
-        this._data = data;
-        this._options = options;
+  public constructor(
+    file: string,
+    data: Storable,
+    options: ConfigOptions = DEFAULT_CONFIG,
+  ) {
+    this._file = file;
+    this._data = data;
+    this._options = options;
+  }
+
+  public get file(): string {
+    return this._file;
+  }
+
+  public has(key: Key): boolean {
+    return util.search(this._data, key) !== undefined;
+  }
+
+  @sync
+  public set<T>(key: Key, value: Storable | T): void {
+    util.set(this._data, key, value);
+  }
+
+  @sync
+  public setBulk<T>(items: { [key: string]: Storable | T }): void {
+    for (const key in items) {
+      util.set(this._data, key, items[key]);
     }
+  }
 
-    public get file(): string {
-        return this._file;
+  public get<T>(key: Key, defaultValue?: T): T | undefined {
+    const value = util.search(this._data, key);
+
+    return value === undefined ? defaultValue : (value as T);
+  }
+
+  public keys(key?: Key): Array<string> {
+    return Object.keys(key ? util.search(this._data, key) || {} : this._data);
+  }
+
+  public all(): Storable {
+    return this._data;
+  }
+
+  @sync
+  public delete(key: Key): void {
+    util.remove(this._data, key);
+  }
+
+  @sync
+  public deleteBulk(keys: Array<Key>): void {
+    for (const key of keys) {
+      util.remove(this._data, key);
     }
+  }
 
-    public has(key: Key): boolean {
-        return util.search(this._data, key) !== undefined;
-    }
-
-    @sync
-    public set<T>(key: Key, value: Storable | T): void {
-        util.set(this._data, key, value);
-    }
-
-    @sync
-    public setBulk<T>(items: { [key: string]: Storable | T }): void {
-        for (const key in items) {
-            util.set(this._data, key, items[key]);
-        }
-    }
-
-    public get<T>(key: Key, defaultValue?: T): T | undefined {
-        const value = util.search(this._data, key);
-
-        return value === undefined ? defaultValue : value as T;
-    }
-
-    public keys(key?: Key): Array<string> {
-        return Object.keys(
-            key ? util.search(this._data, key) || {} : this._data,
-        );
-    }
-
-    public all(): Storable {
-        return this._data;
-    }
-
-    @sync
-    public delete(key: Key): void {
-        util.remove(this._data, key);
-    }
-
-    @sync
-    public deleteBulk(keys: Array<Key>): void {
-        for (const key of keys) {
-            util.remove(this._data, key);
-        }
-    }
-
-    @sync
-    public purge(): void {
-        this._data = {};
-    }
+  @sync
+  public purge(): void {
+    this._data = {};
+  }
 }
--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -2,11 +2,11 @@ import { app } from 'electron';
 import { expect } from 'chai';
 import { join } from 'path';
 import {
-    mkdirSync,
-    rmdirSync,
-    unlinkSync,
-    readFileSync,
-    writeFileSync,
+  mkdirSync,
+  rmdirSync,
+  unlinkSync,
+  readFileSync,
+  writeFileSync,
 } from 'fs';
 import * as utils from './utils';
 import Storable from './Storable';
@@ -14,176 +14,178 @@ import Storable from './Storable';
 const tmpDir = app.getPath('temp');
 
 function unlinkTmpFiles() {
-    try {
-        unlinkSync(join(tmpDir, 'iexist'));
-    } catch(e) {}
+  try {
+    unlinkSync(join(tmpDir, 'iexist'));
+  } catch (e) {}
 
-    try {
-        unlinkSync(join(tmpDir, 'iDONTexist'));
-    } catch (e) {}
+  try {
+    unlinkSync(join(tmpDir, 'iDONTexist'));
+  } catch (e) {}
 
-    try {
-        rmdirSync(join(tmpDir, 'IamAdir'));
-    } catch (e) {}
+  try {
+    rmdirSync(join(tmpDir, 'IamAdir'));
+  } catch (e) {}
 }
 
 describe('utils.sync', () => {
-    afterEach(unlinkTmpFiles);
+  afterEach(unlinkTmpFiles);
 
-    it('updates the file with given object', () => {
-        const path = join(tmpDir, 'iexist');
-        const data: Storable = {
-            first: 'level',
-            deep: { nested: 'value' },
-        };
+  it('updates the file with given object', () => {
+    const path = join(tmpDir, 'iexist');
+    const data: Storable = {
+      first: 'level',
+      deep: { nested: 'value' },
+    };
 
-        utils.sync(path, data);
+    utils.sync(path, data);
 
-        const content = readFileSync(path).toString();
-        expect(content).to.equals(JSON.stringify(data));
-    });
-    it('updates the file with given object - pretty JSON (indent default)', () => {
-        const path = join(tmpDir, 'iexist');
-        const data: Storable = {
-          first: 'level',
-          deep: { nested: 'value' },
-        };
-    
-        const defaultIndent = 2;
-    
-        utils.sync(path, data, { enabled: true });
-    
-        const content = readFileSync(path).toString();
-        expect(content).to.equals(JSON.stringify(data, null, defaultIndent));
-    });
-  
-    it('updates the file with given object - pretty JSON (indent 4)', () => {
-        const path = join(tmpDir, 'iexist');
-        const data: Storable = {
-          first: 'level',
-          deep: { nested: 'value' },
-        };
-    
-        const indent = 4;
-    
-        utils.sync(path, data, { enabled: true, indentSize: indent });
-    
-        const content = readFileSync(path).toString();
-        expect(content).to.equals(JSON.stringify(data, null, indent));
-    });
+    const content = readFileSync(path).toString();
+    expect(content).to.equals(JSON.stringify(data));
+  });
+  it('updates the file with given object - pretty JSON (indent default)', () => {
+    const path = join(tmpDir, 'iexist');
+    const data: Storable = {
+      first: 'level',
+      deep: { nested: 'value' },
+    };
+
+    const defaultIndent = 2;
+
+    utils.sync(path, data, { enabled: true });
+
+    const content = readFileSync(path).toString();
+    expect(content).to.equals(JSON.stringify(data, null, defaultIndent));
+  });
+
+  it('updates the file with given object - pretty JSON (indent 4)', () => {
+    const path = join(tmpDir, 'iexist');
+    const data: Storable = {
+      first: 'level',
+      deep: { nested: 'value' },
+    };
+
+    const indent = 4;
+
+    utils.sync(path, data, { enabled: true, indentSize: indent });
+
+    const content = readFileSync(path).toString();
+    expect(content).to.equals(JSON.stringify(data, null, indent));
+  });
 });
 
 describe('utils.read', () => {
-    afterEach(unlinkTmpFiles);
+  afterEach(unlinkTmpFiles);
 
-    it('returns an empty object if file does not exists', () => {
-        const path = join(tmpDir, 'iDONTexist');
-        expect(utils.read(path)).to.deep.equals({});
-    });
+  it('returns an empty object if file does not exists', () => {
+    const path = join(tmpDir, 'iDONTexist');
+    expect(utils.read(path)).to.deep.equals({});
+  });
 
-    it('returns an the stored object if file exists', () => {
-        const path = join(tmpDir, 'iexist');
-        const data: Storable = { someData: "hey!" };
+  it('returns an the stored object if file exists', () => {
+    const path = join(tmpDir, 'iexist');
+    const data: Storable = { someData: 'hey!' };
 
-        writeFileSync(path, JSON.stringify(data));
+    writeFileSync(path, JSON.stringify(data));
 
-        expect(utils.read(path)).to.deep.equals(data);
-    });
+    expect(utils.read(path)).to.deep.equals(data);
+  });
 
-    it('throws if another error happen', () => {
-        const path = join(tmpDir, 'IamAdir');
-        mkdirSync(path);
-        expect(() => { utils.read(path) }).to.throw(Error);
-    });
+  it('throws if another error happen', () => {
+    const path = join(tmpDir, 'IamAdir');
+    mkdirSync(path);
+    expect(() => {
+      utils.read(path);
+    }).to.throw(Error);
+  });
 });
 
 describe('utils.search', () => {
-    const data: Storable = {
-        first: 'level',
-        answer: 42,
-        deep: { nested: 'value' },
-    };
+  const data: Storable = {
+    first: 'level',
+    answer: 42,
+    deep: { nested: 'value' },
+  };
 
-    it('returns a top level item', () => {
-        expect(utils.search<number>(data, 'answer')).to.equals(42);
-    });
+  it('returns a top level item', () => {
+    expect(utils.search<number>(data, 'answer')).to.equals(42);
+  });
 
-    it('returns a deep nested item', () => {
-        expect(utils.search<string>(data, 'deep.nested')).to.equals('value');
-    });
+  it('returns a deep nested item', () => {
+    expect(utils.search<string>(data, 'deep.nested')).to.equals('value');
+  });
 
-    it('returns undefined for a non existing item', () => {
-        expect(utils.search(data, 'unexistant')).to.be.undefined;
-    });
+  it('returns undefined for a non existing item', () => {
+    expect(utils.search(data, 'unexistant')).to.be.undefined;
+  });
 });
 
 describe('utils.set', () => {
-    it('sets a top level value', () => {
-        const data: Storable = {
-            first: 'level',
-            deep: { nested: 'value' },
-        };
+  it('sets a top level value', () => {
+    const data: Storable = {
+      first: 'level',
+      deep: { nested: 'value' },
+    };
 
-        utils.set(data, 'foo', 'bar');
-        expect(data?.foo).to.equal('bar');
-    });
+    utils.set(data, 'foo', 'bar');
+    expect(data?.foo).to.equal('bar');
+  });
 
-    it('sets a nested value', () => {
-        const data: Storable = {
-            first: 'level',
-            deep: { nested: 'value' },
-        };
+  it('sets a nested value', () => {
+    const data: Storable = {
+      first: 'level',
+      deep: { nested: 'value' },
+    };
 
-        utils.set(data, 'deep.super.nested', 'bar');
-        expect(data.deep?.super?.nested).to.equal('bar');
-    });
+    utils.set(data, 'deep.super.nested', 'bar');
+    expect(data.deep?.super?.nested).to.equal('bar');
+  });
 });
 
 describe('utils.remove', () => {
-    it('removes a top level value', () => {
-        const data: Storable = {
-            first: 'level',
-            deep: { nested: 'value' },
-        };
+  it('removes a top level value', () => {
+    const data: Storable = {
+      first: 'level',
+      deep: { nested: 'value' },
+    };
 
-        utils.remove(data, 'first');
-        expect(data.first).to.be.undefined;
-    });
+    utils.remove(data, 'first');
+    expect(data.first).to.be.undefined;
+  });
 
-    it('removes a nested value', () => {
-        const data: Storable = {
-            first: 'level',
-            deep: { nested: 'value' },
-        };
+  it('removes a nested value', () => {
+    const data: Storable = {
+      first: 'level',
+      deep: { nested: 'value' },
+    };
 
-        utils.remove(data, 'deep.nested');
-        expect(data.deep.nested).to.be.undefined;
-    });
+    utils.remove(data, 'deep.nested');
+    expect(data.deep.nested).to.be.undefined;
+  });
 
-    it('does nothing on an absent value', () => {
-        const data: Storable = {
-            first: 'level',
-            deep: { nested: 'value' },
-        };
+  it('does nothing on an absent value', () => {
+    const data: Storable = {
+      first: 'level',
+      deep: { nested: 'value' },
+    };
 
-        utils.remove(data, 'not.Existing');
-        expect(data).to.deep.equal(data);
-    });
+    utils.remove(data, 'not.Existing');
+    expect(data).to.deep.equal(data);
+  });
 });
 
 describe('utils.pathiffy', () => {
-    it('returns the same array if key is already an array', () => {
-        const key = ['foo', 'bar'];
-        expect(utils.pathiffy(key)).to.deep.equal(key);
-    });
+  it('returns the same array if key is already an array', () => {
+    const key = ['foo', 'bar'];
+    expect(utils.pathiffy(key)).to.deep.equal(key);
+  });
 
-    it('returns an array if key is a dotted string', () => {
-        const key = 'foo.bar';
-        expect(utils.pathiffy(key)).to.deep.equal(['foo', 'bar']);
-    });
+  it('returns an array if key is a dotted string', () => {
+    const key = 'foo.bar';
+    expect(utils.pathiffy(key)).to.deep.equal(['foo', 'bar']);
+  });
 
-    it('returns an array with a single entry if key is a simple string', () => {
-        const key = 'foo';
-        expect(utils.pathiffy(key)).to.deep.equal(['foo']);
-    });
+  it('returns an array with a single entry if key is a simple string', () => {
+    const key = 'foo';
+    expect(utils.pathiffy(key)).to.deep.equal(['foo']);
+  });
 });
